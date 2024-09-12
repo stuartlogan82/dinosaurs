b04cc704e736daa078c220540012af64868e4e7e
stuartlogan82 <stuart.logan@outlook.com>
2024-09-12 10:39:05 +0100

    init commit and configured sanity studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'bfkgtcoi',
+      dataset: 'production',
+      useCdn: false,
+      apiVersion: '2024-09-12',
+      studioBasePath: '/studio',
+  }), 
+  react(),
+]
+});
