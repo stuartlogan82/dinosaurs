5f9ece03484e8bf017d8aa4daa7db0b8765c0ed2
stuartlogan82 <stuart.logan@outlook.com>
2024-09-13 09:29:27 +0100

    added icons in studio for visual styling

--- src/sanity/structure/index.js
+++ src/sanity/structure/index.js
@@ -1,4 +1,5 @@
 // sanity.config.js
+import { GiDinosaurBones, GiCalendar, GiFoodChain, GiPalmTree, GiBookshelf, GiPencil, GiTreasureMap, GiDinosaurEgg } from "react-icons/gi";
 
 export const structure = (S) =>
   S.list()
@@ -6,15 +7,18 @@ export const structure = (S) =>
 .items([
   S.listItem()
     .title('Dinosaurs')
+    .icon(GiDinosaurBones)
     .child(
       S.list()
         .title('Dinosaurs')
         .items([
           S.listItem()
             .title('All Dinosaurs')
+            .icon(GiDinosaurBones)
             .child(S.documentTypeList('dinosaur').title('All Dinosaurs')),
           S.listItem()
             .title('By Period')
+            .icon(GiCalendar)
             .child(
               S.documentTypeList('period')
                 .title('Periods')
@@ -27,6 +31,7 @@ export const structure = (S) =>
             ),
           S.listItem()
             .title('By Diet')
+            .icon(GiFoodChain)
             .child(
               S.documentTypeList('diet')
                 .title('Diets')
@@ -39,6 +44,7 @@ export const structure = (S) =>
             ),
           S.listItem()
             .title('By Habitat')
+            .icon(GiPalmTree)
             .child(
               S.documentTypeList('habitat')
                 .title('Habitats')
@@ -54,17 +60,18 @@ export const structure = (S) =>
   S.divider(),
   S.listItem()
     .title('Taxonomy')
+    .icon(GiBookshelf)
     .child(
       S.list()
         .title('Taxonomy')
         .items([
-          S.documentTypeListItem('period').title('Periods'),
-          S.documentTypeListItem('diet').title('Diets'),
-          S.documentTypeListItem('habitat').title('Habitats'),
-          S.documentTypeListItem('dinosaurGroup').title('Dinosaur Groups'),
+          S.documentTypeListItem('period').title('Periods').icon(GiCalendar),
+          S.documentTypeListItem('diet').title('Diets').icon(GiFoodChain),
+          S.documentTypeListItem('habitat').title('Habitats').icon(GiPalmTree),
+          S.documentTypeListItem('dinosaurGroup').title('Dinosaur Groups').icon(GiDinosaurEgg),
         ])
     ),
   S.divider(),
-  S.documentTypeListItem('location').title('Locations'),
-  S.documentTypeListItem('dinosaurOutline').title('Dinosaur Outlines'),
+  S.documentTypeListItem('location').title('Locations').icon(GiTreasureMap),
+  S.documentTypeListItem('dinosaurOutline').title('Dinosaur Outlines').icon(GiPencil),
 ])
